b3c0871fc3623b0e7d02dd8bcc097888949ea357
advance512 <diamant.alon@gmail.com>
2019-04-15 23:15:44 +0300

    Fixed preparation and segmentation scripts, supports Python 3.6+ now
    Added .gitignore
    Fixed: prepare_data.py
    Fixed: segment_class.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,47 +1,118 @@
-import sys
-import os
 import argparse
+import os
+import shutil
 from glob import glob
 from random import shuffle
-import shutil
 
-def prepare(img_dir, seg_dir, img_target_dir, seg_target_dir, replace_names=None):
-  imgs = set(glob(img_dir+"*.png"))
-  segs = set(glob(seg_dir+"*.png"))
-  pairs = []
-  for img_path in list(imgs):
-    seg_path = seg_dir + (img_path.split("/")[-1].replace(replace_names[0], replace_names[1]) if replace_names else img_path.split("/")[-1])
-    if seg_path in segs:
-      pairs.append((img_path, seg_path))
-  print "candidates:", len(pairs)
-  if len(pairs) < args["train_size"] + args["test_size"]:
-    print "candidates not enough!"
-    return
-  if not os.path.exists(img_target_dir):
-    os.makedirs(img_target_dir)
-    os.makedirs(img_target_dir.replace("train", "test"))
-  if not os.path.exists(seg_target_dir):
-    os.makedirs(seg_target_dir)
-    os.makedirs(seg_target_dir.replace("train", "test"))
-  shuffle(pairs)
-  for i in range(args["train_size"]):
-    shutil.copy2(pairs[i][0], img_target_dir + (pairs[i][0].split("/")[-1].replace(replace_names[0], "") if replace_names else ""))
-    shutil.copy2(pairs[i][1], seg_target_dir + (pairs[i][1].split("/")[-1].replace(replace_names[1], "") if replace_names else ""))
-  for i in range(args["train_size"], args["train_size"]+args["test_size"]):
-    shutil.copy2(pairs[i][0], img_target_dir.replace("train", "test") + (pairs[i][0].split("/")[-1].replace(replace_names[0], "") if replace_names else ""))
-    shutil.copy2(pairs[i][1], seg_target_dir.replace("train", "test") + (pairs[i][1].split("/")[-1].replace(replace_names[1], "") if replace_names else ""))
+def copy_file(fromFile, toDirectory, replace_names=None):
+    fromFileName = fromFile.split("/")[-1]
+    if replace_names:
+        for name in replace_names:
+            fromFileName = fromFileName.replace(name, "")
+    toFile = toDirectory + fromFileName
+    print("Copying from: %s to: %s" % (fromFile, toFile))
+    shutil.copy2(
+        fromFile,
+        toFile
+    )
+
+def verify_dir_exists(dirPath):
+    if not os.path.exists(dirPath):
+        print("Making directory: %s" % dirPath)
+        os.makedirs(dirPath)
+
+def prepare(img_dir, seg_dir, img_target_dir, seg_target_dir, trainSize, testSize, replace_names=None):
+
+    imgs = set(glob(img_dir + "*.png"))
+    segs = set(glob(seg_dir + "*.png"))
+
+    # Find pairs
+    pairs = []
+    for img_path in list(imgs):
+        seg_path = seg_dir + (img_path.split("/")[-1].replace(replace_names[0], replace_names[1]) if replace_names else
+                              img_path.split("/")[-1])
+        if seg_path in segs:
+            pairs.append((img_path, seg_path))
+        else:
+            print("Could not find segmentation image file %s" % seg_path)
+    print("candidate pairs:i ", len(pairs))
+
+    if len(pairs) < trainSize + testSize:
+        print("%s candidates not enough! need at least %s" % (len(pairs), trainSize + testSize))
+        return
+
+    # Directories
+    train_img_target_dir = img_target_dir
+    train_seg_target_dir = seg_target_dir
+    test_img_target_dir = img_target_dir.replace("train", "test")
+    test_seg_target_dir = seg_target_dir.replace("train", "test")
+
+    # Create target dirs
+    verify_dir_exists(train_img_target_dir)
+    verify_dir_exists(train_seg_target_dir)
+    verify_dir_exists(test_img_target_dir)
+    verify_dir_exists(test_seg_target_dir)
+    shuffle(pairs)
+
+    # Copy in the atual images
+    for i in range(trainSize):
+       copy_file(
+           fromFile=pairs[i][0],
+           toDirectory=train_img_target_dir,
+           replace_names=replace_names
+       )
+       copy_file(
+           fromFile=pairs[i][1],
+           toDirectory=train_seg_target_dir,
+           replace_names=replace_names
+       )
+
+    for i in range(trainSize, trainSize + testSize):
+        copy_file(
+            fromFile=pairs[i][0],
+            toDirectory=test_img_target_dir,
+            replace_names=replace_names
+        )
+        copy_file(
+            fromFile=pairs[i][1],
+            toDirectory=test_seg_target_dir,
+            replace_names=replace_names
+        )
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--A_imagepath", "-Ai", type=str, default="/home/lpl/data/playing/images/", help="dataset A's image path")
-  parser.add_argument("--A_segpath", "-As", type=str, default="/home/lpl/data/playing/labels/", help="dataset A's segmentation path")
-  # cp `find train/ -name "*.png"` all_train/
-  parser.add_argument("--B_imagepath", "-Bi", type=str, default="/home/lpl/data/cityscape/leftImg8bit/all_train/", help="dataset B's image path")
-  parser.add_argument("--B_segpath", "-Bs", type=str, default="/home/lpl/data/cityscape/gtFine/all_train/", help="dataset B's segmentation path")
-  parser.add_argument("--train_size", "-tr", type=int, default=2000, help="number of training examples for each dataset")
-  parser.add_argument("--test_size", "-te", type=int, default=500, help="number of test examples for each dataset")
-  args = vars(parser.parse_args())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--A_imagepath", "-Ai", type=str, default="/home/alon/data/playing/images/",
+                        help="dataset A's image path")
+    parser.add_argument("--A_segpath", "-As", type=str, default="/home/alon/data/playing/labels/",
+                        help="dataset A's segmentation path")
+    # cp `find train/ -name "*.png"` all_train/
+    parser.add_argument("--B_imagepath", "-Bi", type=str, default="/home/alon/data/cityscape/leftImg8bit/all_train/",
+                        help="dataset B's image path")
+    parser.add_argument("--B_segpath", "-Bs", type=str, default="/home/alon/data/cityscape/gtFine/all_train/",
+                        help="dataset B's segmentation path")
+    parser.add_argument("--train_size", "-tr", type=int, default=2000,
+                        help="number of training examples for each dataset")
+    parser.add_argument("--test_size", "-te", type=int, default=500, help="number of test examples for each dataset")
+    args = vars(parser.parse_args())
 
-  prepare(args["A_imagepath"], args["A_segpath"], "./datasets/gta/trainA/", "./datasets/gta/trainA_seg/")
-  prepare(args["B_imagepath"], args["B_segpath"], "./datasets/gta/trainB/", "./datasets/gta/trainB_seg/", replace_names=("_leftImg8bit", "_gtFine_color"))
+    trainSize = args["train_size"]
+    testSize = args["test_size"]
+
+    prepare(
+        img_dir=args["A_imagepath"],
+        seg_dir=args["A_segpath"],
+        img_target_dir="./datasets/gta/trainA/",
+        seg_target_dir="./datasets/gta/trainA_seg/",
+        trainSize=trainSize,
+        testSize=testSize,
+    )
+    prepare(
+        img_dir=args["B_imagepath"],
+        seg_dir=args["B_segpath"],
+        img_target_dir="./datasets/gta/trainB/",
+        seg_target_dir="./datasets/gta/trainB_seg/",
+        trainSize=trainSize,
+        testSize=testSize,
+        replace_names=("_leftImg8bit", "_gtFine_color")
+    )
 
--- segment_class.py
+++ segment_class.py
@@ -1,10 +1,12 @@
-from collections import defaultdict
 import os
-import sys
+from collections import defaultdict
 from glob import glob
-from scipy.misc import imread, imsave, toimage
-import numpy as np
 from multiprocessing import Pool as ProcessPool
+
+import numpy as np
+from scipy.misc import imread, toimage
+
+
 num_seg_masks = 8
 # vehicles: 1
 # pedestrians: 2
@@ -16,7 +18,8 @@ num_seg_masks = 8
 # others: 0
 
 
-# https://bitbucket.org/visinf/projects-2016-playing-for-data/src/6afee1a5923f452e741c9256f5fb78f2b3882ee2/label/initLabels.m?at=master&fileviewer=file-view-default
+# https://bitbucket.org/visinf/projects-2016-playing-for-data/src/6afee1a5923f452e741c9256f5fb78f2b3882ee2/label
+# /initLabels.m?at=master&fileviewer=file-view-default
 """
 ('0,0,0', 'unlabeled')
 ('0,0,0', 'ego vehicle')
@@ -54,43 +57,74 @@ num_seg_masks = 8
 ('119,11,32', 'bicycle')
 ('0,0,142', 'license plate')
 """
+
+
 # https://github.molgen.mpg.de/mohomran/cityscapes/blob/master/scripts/helpers/labels.py
 def cityscape():
-  rgb_to_maskidx = defaultdict(int)
-  maps = [((128,64,128),4), ((244,35,232),4), ((250,170,160),4), ((230,150,140),4), ((70,70,70),5)
-         , ((102,102,156),5), ((190,153,153),5), ((180,165,180),5), ((150,100,100),5), ((150,120,90),5)
-         , ((107,142,35),7), ((70,130,180),6), ((220,20,60),2), ((255,0,0),2), ((0,0,142),1), ((0,0,70),1)
-         , ((0,60,100),1), ((0,0,90),1), ((0,0,110),1), ((0,0,230),3), ((119,11,32),3)]
-  for k,v in maps:
-    rgb_to_maskidx[k] = v
-  return rgb_to_maskidx 
+    rgb_to_maskIndex = defaultdict(int)
+    rgb_to_maskPairs = [
+        ((128, 64, 128), 4),
+        ((244, 35, 232), 4),
+        ((250, 170, 160), 4),
+        ((230, 150, 140), 4),
+        ((70, 70, 70), 5),
+        ((102, 102, 156), 5),
+        ((190, 153, 153), 5),
+        ((180, 165, 180), 5),
+        ((150, 100, 100), 5),
+        ((150, 120, 90), 5),
+        ((107, 142, 35), 7),
+        ((70, 130, 180), 6),
+        ((220, 20, 60), 2),
+        ((255, 0, 0), 2),
+        ((0, 0, 142), 1),
+        ((0, 0, 70), 1),
+        ((0, 60, 100), 1),
+        ((0, 0, 90), 1),
+        ((0, 0, 110), 1),
+        ((0, 0, 230), 3),
+        ((119, 11, 32), 3)
+    ]
+
+    for k, v in rgb_to_maskPairs:
+        rgb_to_maskIndex[k] = v
+    return rgb_to_maskIndex
 
 def A_maskmap():
-  return cityscape()
+    return cityscape()
 
 def B_maskmap():
-  return cityscape()
+    return cityscape()
 
-def preprocess_master(src):
-  dst = src.replace("_seg", "_seg_class")
-  if not os.path.exists(dst):
-    os.makedirs(dst)
-  segs = set(glob(src+"/*.png"))
-  pool = ProcessPool(8)
-  pool.map(preprocess, segs)
- 
-def preprocess(image_seg):
-  base_name = os.path.basename(image_seg)
-  print "processing", base_name
-  img = imread(image_seg)
-  M, N = img.shape[:2]
-  seg_class = np.zeros((M, N)).astype(np.int)
-  for x in range(M):
-    for y in range(N):
-      seg_class[x,y] = maskmap[tuple(img[x,y,:3])]
-  toimage(seg_class, cmin=0, cmax=255).save(image_seg.replace("_seg", "_seg_class")) 
+# Preprocess images - creating segment maps for SG-GAN
+def preprocess(image_seg_tuple):
+    (index, image_seg, maskmap) = image_seg_tuple
+    base_name = os.path.basename(image_seg)
+    print("Processing image #%s: %s" % (index, base_name))
+    img = imread(image_seg)
+    M, N = img.shape[:2]
+    seg_class = np.zeros((M, N)).astype(np.int)
+    for x in range(M):
+        for y in range(N):
+            seg_class[x, y] = maskmap[tuple(img[x, y, :3])]
+    toimage(seg_class, cmin=0, cmax=255).save(image_seg.replace("_seg", "_seg_class"))
+
+# Run 8 process, preprocessing images
+def preprocess_master(src, maskmap):
+    dst = src.replace("_seg", "_seg_class")
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    segs = set(glob(src + "/*.png"))
+    segs = list(
+        (idx, image_seg, maskmap)
+        for (idx, image_seg) in enumerate(segs)
+    )
+    pool = ProcessPool(8)
+    pool.map(preprocess, segs)
 
 if __name__ == "__main__":
-  maskmap = A_maskmap()
-  preprocess_master("datasets/gta/trainA_seg")
-  preprocess_master("datasets/gta/trainB_seg")
+    maskmapA = A_maskmap()
+    preprocess_master("datasets/gta/trainA_seg", maskmapA)
+    maskmapB = A_maskmap()
+    preprocess_master("datasets/gta/trainB_seg", maskmapB)
+
